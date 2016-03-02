6b046cb87ba675cf0c941d12982a52340a8ed605
frjurado <clases.max@gmail.com>
2016-03-02 12:51:25 +0100

    2d Flask-Migrate

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,13 +1,16 @@
 import unittest
-from app import create_app
+from app import create_app, db
 
 class BasicTestCase(unittest.TestCase):
     def setUp(self):
         self.app = create_app('testing')
         self.app_context = self.app.app_context()
         self.app_context.push()
+        db.create_all()
 
     def tearDown(self):
+        db.session.remove()
+        db.drop_all()
         self.app_context.pop()
 
     def test_app_exists(self):
